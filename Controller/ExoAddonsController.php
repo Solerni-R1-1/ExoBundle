f0a4b3141750ed6793be2d456516577447ddfd06
Kevin Danezis <kdanezis@sii.fr>
2014-05-14 15:49:12 +0200

    change page Paper

--- Controller/ExoAddonsController.php
+++ Controller/ExoAddonsController.php
@@ -40,4 +40,75 @@ class ExoAddonsController extends Controller
             
         }
         
+        public function getLessonInSessionAction(AbstractWorkspace $workspace){
+             
+            $em = $this->getDoctrine()->getManager();
+            $session = $this->getRequest()->getSession();
+            //get the first lesson
+            $lesson = $this->getFirstLessonFromWorkspace($workspace);
+            
+      
+            
+            return $this->render(
+                'UJMExoBundle:Partial:titleLesson.html.twig',
+                array(
+                    'name'     => $lesson->getResourceNode()->getName()
+                )
+            );
+        
+        }
+        
+        /**
+         * @param AbstractWorkspace $workspace
+         *
+         * @return \Claroline\CoreBundle\Entity\Resource\ResourceNode
+         */
+        protected function getFirstLessonFromWorkspace(AbstractWorkspace $workspace)
+        {
+            $lessonRepository = $this->getDoctrine()->getRepository('IcapLessonBundle:Lesson');
+            $resource = $this->getFirstResourceFromWorkspace($workspace, 'icap_lesson');
+        
+            if ($resource != null) {
+                return $lessonRepository->findOneByResourceNode($resource);
+            }
+            return $resource;
+        }
+        
+        /**
+         * Return the first element of the required type
+         *
+         * @param AbstractWorkspace $workspace
+         * @param string $resourceName the name of th resource
+         * @param string $mimeType (optionnal) the mime type. Accept wildcards.
+         *
+         * @return \Claroline\CoreBundle\Entity\Resource\ResourceNode
+         */
+        private function getFirstResourceFromWorkspace(AbstractWorkspace $workspace, $resourceName, $mimeType = null)
+        {
+            $doctrine = $this->getDoctrine();
+            $resourcesNodeRepository = $doctrine->getRepository('ClarolineCoreBundle:Resource\ResourceNode');
+        
+            $rootResource = $resourcesNodeRepository->findWorkspaceRoot($workspace);
+            $current = $resourcesNodeRepository->findOneBy(array(
+                'parent' => $rootResource,
+                'previous' => null
+            ));
+            while (
+                $current !== null
+                && (
+                    $current->getResourceType()->getName() != $resourceName
+                    || (
+                        ($mimeType !== null)
+                        && (!preg_match('#'.$mimeType.'#', $current->getMimeType())
+                        )
+                    )
+                )
+            ) {
+                $current = $current->getNext();
+            }
+        
+        
+            return $current;
+        }
+        
 }
